746d51d76d8381663cdba9acaad30a8de05c961c
Vincent De Borger <vincent.deborger@gmail.com>
2025-12-20 10:13:49 +0100

    feat: initial commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # dotfiles
 My personal collection of config files for my Linux environments 
+
+## Prerequisites
+
+- [neovim](https://neovim.io): `rpm-ostree install nvim`
+- [vicinae](https://github.com/vicinaehq/vicinae): `sudo dnf copr enable scottames/vicinae && rpm-ostree install vicinae`
+- [hypridle](https://github.com/hyprwm/hypridle): `rpm-ostree install hypridle` 
+- [hyprlock](https://github.com/hyprwm/hyprlock): `rpm-ostree install hyprlock`
+- [waybar](https://github.com/Alexays/Waybar): `rpm-ostree install waybar`
+- [JetBrains Mono](https://www.jetbrains.com/lp/mono/): `rpm-ostree install jetbrains-mono-fonts`
+- [nwg-look](https://github.com/nwg-piotr/nwg-look): `rpm-ostree install nwg-look`
---
+++ nvim/lua/vdeborger/remap.lua
@@ -0,0 +1,41 @@
+vim.g.mapleader = " "
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv") -- Move selected block down
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv") -- Move selected block up
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz") -- Scroll down and center cursor
+vim.keymap.set("n", "<C-u>", "<C-u>zz") -- Scroll up and center cursor
+
+vim.keymap.set("n", "n", "nzzzv") -- Move to next search result and center cursor
+vim.keymap.set("n", "N", "Nzzzv") -- Move to previous search result and center cursor
+
+-- <C-s> to search/replace the current word object (saves to h register)
+vim.keymap.set("n", "<C-s>", '"hyiw:%s/<C-r>h//g<left><left>', { noremap = true })
+
+-- System clipboard copy/pasting
+vim.keymap.set("v", "<leader>yy", '"+y')
+vim.keymap.set({ "n", "v" }, "<leader>pp", '"+p')
+-- Primary selection
+vim.keymap.set("v", "<leader>y1", '"*y')
+vim.keymap.set({ "n", "v" }, "<leader>p1", '"*p')
+-- Whole file
+vim.keymap.set("n", "<leader>ya", ":%y+<cr>")
+vim.keymap.set("n", "<leader>pa", 'ggVG"+p')
+
+vim.keymap.set("x", "<leader>p", [["_dP]]) -- Paste over selected text without overwriting register
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]]) -- Delete selected text without overwriting register
+
+-- Buffers (tabs)
+vim.keymap.set("n", "<Tab>", "<Cmd>BufferNext<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<S-Tab>", "<Cmd>BufferPrevious<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>q", "<Cmd>BufferClose<CR>", { noremap = true, silent = true })
+
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format) -- Format current buffer using LSP
+
+-- Tmux
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>") -- New session
+
+-- Reload current file
+vim.keymap.set("n", "<leader><leader>", function()
+    vim.cmd("so")
+end)
